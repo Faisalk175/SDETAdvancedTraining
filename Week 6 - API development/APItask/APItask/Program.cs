29494e07e43ed4d3cdbce90fe3d5f203bc97eff3
Faisalk175 <faisalk3412@gmail.com>
2023-01-20 17:41:49 +0000

    Lab tests passing, APIClinet

---
+++ Week 6 - API development/APItask/APItask/Program.cs
@@ -0,0 +1,31 @@
+﻿using RestSharp;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+using Newtonsoft.Json.Linq;
+
+namespace APItask;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        var restClient = new RestClient("https://api-football-standings.azharimm.site/");
+
+        var restRequest = new RestRequest();
+        //build the requrest
+        restRequest.Method = Method.Get;
+        restRequest.AddHeader("Content-Type", "application/json");
+        restRequest.Timeout = -1;
+
+        string football = "leagues";
+
+        restRequest.Resource = football;
+
+        var Response =  restClient.Execute(restRequest);
+
+        Console.WriteLine(Response.Content);
+        
+
+        
+    }
+}
---
+++ Week 6 - API development/LambdasAndLinq/LamdaAndLinq/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Diagnostics;
+using System.Xml;
+
+namespace LambdasAndLinq;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        //Language Integrate Queries
+        var nums = new List<int> { 3, 7, 1, 2, 8, 3, 0, 4, 5 };
+
+        var numsCount = nums.Count();
+
+        //int countEven = 0;
+        //foreach(int num in nums)
+        //{
+        //    if (IsEven(num)) countEven++;
+        //}
+
+        int countEven = nums.Count(IsEven);
+
+        List<Person> people = new List<Person> {
+           new Person { Name = "Cathy", Age = 40, City = "Ottawa"},
+           new Person { Name = "Nish", Age = 55,City = "Birmingham"},
+           new Person { Name = "Martin", Age = 20, City = "London"}
+        };
+
+        var countYoungPeople = people.Count(IsYoung);
+
+        //int sumOfEven = nums.Sum(IsEven);
+
+        // anonymous method using delegates
+        int countDEven = nums.Count(delegate (int num) { return num % 2 == 0; });
+        
+
+        //lambda expressions
+
+        // given something => return something
+        int sumOfSquares = nums.Sum(x => x * x);
+
+        int countLEven = nums.Count(num => num % 2 == 0);
+
+        var peopleInLondonQuery = people.Where(p => p.City == "London").ToList();
+
+        var peopleByAge = people.OrderBy(p => p.Age);
+
+        foreach(var person in peopleByAge)
+        {
+            Console.WriteLine(person);
+        }
+
+        var namesOfThoseOver20 = people.Where(p => p.Age > 20).Select(p => p.Name).First();
+
+        string newString = ModifyString("Hello World",  s => s.Replace(" ", "_").ToUpper());
+    }
+
+    private static string ModifyString(string str, Func<string, string> strModify)
+    {
+        return strModify(str);
+    }
+
+    private static int Square(int x)
+    {
+        return x * x;
+    }
+
+    private static bool IsEven(int num)
+    {
+        return num % 2 == 0;
+    }
+
+    private static bool IsYoung(Person p)
+    {
+        return p.Age < 30;
+    }
+}
+
+public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public string City { get; set; }
+    public override string ToString()
+    {
+        return $"{Name} - {City} - {Age}";
+    }
+}
+
+
